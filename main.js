68d3be4090e61763e5c4b3e12e7bad26214d8130
xacer <chacecaven@outlook.com>
2023-01-29 20:52:48 -0500

    Finished animation and cleaned code

--- main.js
+++ main.js
@@ -1,190 +1,206 @@
+import * as THREE from "./three/src/Three.js";
 
-import * as THREE from './three/src/Three.js';
-
-import { GLTFLoader } from "./three/examples/jsm/loaders/GLTFLoader.js"
+import { GLTFLoader } from "./three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "./three/examples/jsm/loaders/DRACOLoader.js";
-
-import { OrbitControls } from "./three/examples/jsm/controls/OrbitControls.js";
-
-import { GUI } from "./three/examples/jsm/libs/lil-gui.module.min.js";
-
-import {
-    createAndSetupCanvas,
-    smoothstep
-} from './utils.js';
-import { lerp } from './three/src/math/MathUtils.js';
+import { createAndSetupCanvas, smoothstep } from "./utils.js";
+import { lerp } from "./three/src/math/MathUtils.js";
 
 const canvas = createAndSetupCanvas(600, 600);
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 
-renderer.setClearColor(new THREE.Color(0.0, 0.1, 0.25));
-renderer.clear();
-
 const scene = new THREE.Scene();
 
-const vertexShader = /* glsl */`
-#define ROT2D(theta) mat2(cos(theta), -sin(theta), cos(theta), sin(theta))
-
-uniform float time;
-
-vec3 calculateDisplacement (vec3 pos) {
-
-  vec3 startPoint = 1000.0 * normalize(pos + 100.0 * cos(pos * 534.86));
-
-  float k = max(sin(time * 0.1 - pos.x * 0.002), 0.0);
-
-  return (startPoint - pos) * k;
-}
-
-void main() {
-
-  // transform!
-  vec3 transformedPosition = position.xyz + calculateDisplacement(position.xyz);
-
-  vec4 mvPosition = modelViewMatrix * vec4(transformedPosition, 1.0);
-
-  gl_PointSize = 3.0 * ( 150.0 / -mvPosition.z );
-  gl_Position = projectionMatrix * mvPosition;
-
-}`;
-const fragmentShader = /* glsl */`
-
-uniform sampler2D pointTexture;
-
-vec3 pointColor = 1.0 * vec3(1.0, 1.0, 1.0);
-
-void main() {
-
-  float transparency = texture2D(pointTexture, gl_PointCoord).a;
-
-  vec4 color = vec4( pointColor, 0.05 * transparency );
-
-  gl_FragColor = color;
-}
-`;
-
-const texture = new THREE.TextureLoader().load( './res/disc.png' );
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-
+const texture = new THREE.TextureLoader().load("./res/disc.png");
 
 const material = new THREE.ShaderMaterial({
+    uniforms: {
+        time: { value: 1.914 },
+        animTime: { value: 0.0 },
+        pointTexture: { value: texture },
+    },
 
-  uniforms: {
-    time: { value: 1.914 },
-    pointTexture: { value: texture }
-  },
+    vertexShader: getVertexShader(),
+    fragmentShader: getFragmentShader(),
 
-  vertexShader, fragmentShader,
+    transparent: true,
 
-  transparent: true,
-
-  face: THREE.DoubleSide,
-  blendEquation: THREE.AdditiveBlending,
-  depthFunc: THREE.AlwaysDepth
+    face: THREE.DoubleSide,
+    blendEquation: THREE.AdditiveBlending,
+    depthFunc: THREE.AlwaysDepth,
 });
-
 
 const dracoLoader = new DRACOLoader();
+
 const loader = new GLTFLoader();
-loader.setDRACOLoader(dracoLoader);
-loader.load("./res/polar_bear_points_remesh.glb", gltf => {
-
-  console.log(gltf);
-  
-  const points = gltf.scene.children[0];
-
-  points.material = material;
-
-  points.translateY(-25);
-
-  console.log(points);
-
-  scene.add(points);
-
-  /* do stuff */
-
-});
 
 const camera = new THREE.PerspectiveCamera(75, 1.0, 1.0, 5000.0);
-camera.translateZ(0.0);
-camera.translateY(0.0);
-camera.translateX(-200);
-camera.lookAt(0, 0, 0);
 
-const controls = new OrbitControls(camera, canvas);
+init();
 
-const gui = new GUI();
-const guiParams = {
-  speed: 0.0
-};
+function init() {
 
-gui.add(guiParams, "speed", -1.0, 1.0);
+    loader.setDRACOLoader(dracoLoader);
+    loader.load("./res/polar_bear_points_remesh.glb", (gltf) => {
+        console.log(gltf);
 
-function controlAnimation(animTime) {
+        /** @type {THREE.Points} */
+        const points = gltf.scene.children[0];
 
-  const segmentLengths = [
-    10, 10, 10
-  ];
+        points.material = material;
 
-  let segmentIndex = 0;
-  let segmentTime = animTime;
-  while (segmentTime > segmentLengths[segmentIndex] && segmentIndex < segmentLengths.length - 1) {
-    segmentTime -= segmentLengths[segmentIndex];
-    segmentIndex += 1;
-  }
+        points.translateY(-25);
 
-  const segmentFunctions = [
+        const numPoints = points.geometry.getAttribute("position").count;
+        const rndPoints = new Float32Array(3 * numPoints);
 
-    function (t) {
+        for (let i = 0; i < numPoints; i ++) {
+            rndPoints[i * 3 + 0] = Math.random() * 2000.0 - 1000.0;
+            rndPoints[i * 3 + 1] = Math.random() * 2000.0 - 1000.0;
+            rndPoints[i * 3 + 2] = Math.random() * 2000.0 - 1000.0;
+        }
 
-      t = smoothstep(t);
+        const rndPointAttribute = new THREE.BufferAttribute(rndPoints, 3, false);
+        points.geometry.setAttribute("rndPoint", rndPointAttribute);
 
-      const startTime = 1.914;
-      const endTime = 0.0;
-      material.uniforms["time"].value = lerp(
-        startTime,
-        endTime,
-        t
-      );
+        console.log(points);
 
-    },
+        scene.add(points);
+    });
 
-    function (t) {
+    renderer.setClearColor(new THREE.Color(0.0, 0.1, 0.25));
 
-    },
+    document.body.onkeydown = (keyboardEvent) => {
+        if (keyboardEvent.key.toString() == "p") {
+            console.log(camera.position);
+            console.log(camera.quaternion);
+        }
+    };
 
-    function (t) {
+    let then = 0;
+    let t = 0;
+
+    function render(now) {
+
+        now *= 0.001; // convert to seconds
+        const deltaTime = now - then;
+        then = now;
+
+        requestAnimationFrame(render);
+
+        animate(Math.min(t / 5.0, 1.0));
+
+        renderer.render(scene, camera);
+
+        //controls.update();
+
+        t += deltaTime;
+
+        material.uniforms["time"].value = t;
+        //material.uniformsNeedUpdate = true;
+    }
+
+    requestAnimationFrame(render);
+
+}
+
+function animate(t) {
+    t = smoothstep(t);
+
+    const startTime = 1.914;
+    const endTime = 0.0;
+    material.uniforms["animTime"].value = lerp(startTime, endTime, t);
+
+    const cameraPositions = [
+        // Starting position
+        new THREE.Vector3(200, 0, 0),
+
+        // Ending position
+        new THREE.Vector3(137.21, 61.17, -16.68)
+    ];
+
+    const cameraQuaternions = [
+        // Starting rotation
+        new THREE.Quaternion(
+            0,
+            0.7071067811865476,
+            0,
+            0.7071067811865476
+        ),
+
+        // Ending rotation
+        new THREE.Quaternion(
+            -0.044043449956680664,
+            0.8651926590903987,
+            0.07721638771201698,
+            0.4934971799723947
+        )
+    ];
+
+    // Interpolate positions
+    camera.setRotationFromQuaternion(cameraQuaternions[0].slerp(cameraQuaternions[1], t));
+    
+    const newPosition = cameraPositions[0].lerp(cameraPositions[1], t);
+
+    camera.position.set(newPosition.x, newPosition.y, newPosition.z);
+
+    camera.matrixWorldNeedsUpdate = true;
+
+    // Interpolate rotations
+
+
+
+}
+
+function getVertexShader() {
+    return /* glsl */ `
+    uniform float time;
+    uniform float animTime;
+
+    attribute vec3 rndPoint;
+
+    vec3 displaceA (vec3 pos, float k) {
+
+        vec3 start = rndPoint;
+        return pos + (start - pos) * k;
 
     }
 
-  ];
+    vec3 displaceB (vec3 pos, float k) {
 
-  segmentFunctions[segmentIndex](segmentTime / segmentLengths[segmentIndex]);
+        if (k == 0.0) {
+            float scanline = max(cos(pos.y * 0.1 + time * 5.0) - 0.95, 0.0);
+            pos.xz += normalize(pos.xz) * scanline * 10.0;
+            return pos;
+        }
+
+        return pos;
+
+    }
+
+
+    void main() {
+        vec3 p = position.xyz;
+
+        float k = max(sin(animTime * 0.1 - p.x * 0.002), 0.0);
+
+        p = displaceA(p, k);
+        p = displaceB(p, k);
+
+        vec4 mvPosition = modelViewMatrix * vec4(p, 1.0);
+
+        gl_PointSize = 3.0 * ( 150.0 / -mvPosition.z );
+        gl_Position = projectionMatrix * mvPosition;
+    }`;
 }
 
-let then = 0;
-let animTime = 0;
-function render(now) {
-  now *= 0.001;  // convert to seconds
-  const deltaTime = now - then;
-  then = now;
-
-  requestAnimationFrame(render);
-
-  controlAnimation(animTime);
-
-  renderer.render(scene, camera);
-  
-  controls.update();
-
-  
-  animTime += deltaTime;
-
-  //material.uniforms["time"].value += guiParams.speed * deltaTime;
-  //material.uniformsNeedUpdate = true;
-  
+function getFragmentShader() {
+    return /* glsl */ `
+    uniform sampler2D pointTexture;
+    void main() {
+      float transparency = texture2D(pointTexture, gl_PointCoord).a;
+      vec4 color = vec4( 1.0, 1.0, 1.0, 0.05 * transparency );
+      gl_FragColor = color;
+    }
+    `;
 }
-
-requestAnimationFrame(render);
